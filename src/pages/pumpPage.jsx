d540195fe13970ca9689fd8b6719fc5423839fae
kvisten-e <kvist.emil@gmail.com>
2024-05-12 00:17:41 +0200

    .

---
+++ src/pages/pumpPage.jsx
@@ -0,0 +1,35 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState, useRef, useContext } from 'react';
+import { GlobalContext } from '../components/GlobalContext.jsx';
+import Pump from '../components/pump.jsx'
+
+export default function pumpTokens() {
+  
+  const [seconds, setSeconds] = useState(900)
+  const [triggerPump, setTriggerPump] = useState(false);
+
+  const changeSeconds = (e) => {
+    setSeconds(e.target.value)
+  }
+  const handlePumpTrigger = () => {
+    setTriggerPump(prev => !prev); 
+  };  
+
+  return (
+    <>
+      <div id="main-pump">
+        <div id="first-pump">
+          <h1>Pump tokens</h1>
+          <div>
+            <h3>Seconds to reach Raydium</h3>
+            <input type="number" value={seconds} onChange={changeSeconds} />
+          </div>
+          <button onClick={handlePumpTrigger}>Search for tokens</button>
+        </div>
+        <div id="seconds-pump">
+          <Pump seconds={seconds} triggerAction={triggerPump} />
+        </div>
+      </div>
+    </>
+  );
+}
